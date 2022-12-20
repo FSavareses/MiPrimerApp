45b0f88fcc60bf783b1cdcc783c6292a0d4c7aac
FSavareses <franco_savareses_lsf@hotmail.com>
2022-12-20 16:52:41 -0300

    "primera pantalla Login"

--- app/src/main/java/com/example/miprimerapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimerapp/MainActivity.kt
@@ -1,43 +1,109 @@
 package com.example.miprimerapp
 
+import android.content.Context
 import android.os.Bundle
+import android.text.style.BackgroundColorSpan
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material.icons.filled.Lock
+import androidx.compose.material.icons.filled.Person
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.miprimerapp.ui.theme.MiPrimerAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MiPrimerAppTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            LoginUI(applicationContext)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
+fun LoginUI(context: Context) {
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+    Column() {
+        Text(
+            text = "Iniciar Sesión",
+            fontFamily = FontFamily.SansSerif,
+            textAlign = TextAlign.Left,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Red,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 20.dp)
+        )
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Ingresar Email") },
+            leadingIcon = {
+                Icon(Icons.Default.AccountCircle, contentDescription = "Person")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp)
+        )
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Ingresar Contraseña") },
+            leadingIcon = {
+                Icon(Icons.Default.Lock, contentDescription = "password")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MiPrimerAppTheme {
-        Greeting("Android")
+        OutlinedButton(
+            onClick = { logged(email, password, context)},
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp)
+        ) {
+
+
+            Text(
+                text = "login",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+        }
     }
 }
+
+fun logged(email: String, password: String, context: Context) {
+    if (email  == "john@dot.com" && password == "1234"){
+        Toast.makeText(context, "Correcto inicio de sesión", Toast.LENGTH_SHORT).show()
+
+    }else  {
+        Toast.makeText(context, "inicio de sesión Fallido", Toast.LENGTH_SHORT).show()
+    }
+}
+
