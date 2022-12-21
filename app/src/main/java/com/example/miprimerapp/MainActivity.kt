1c4f25478bc5d95f76acd3acbe8926cfb795b7cc
FSavareses <franco_savareses_lsf@hotmail.com>
2022-12-21 15:14:48 -0300

    "primera pantalla Login Comentarios"

--- app/src/main/java/com/example/miprimerapp/MainActivity.kt
+++ app/src/main/java/com/example/miprimerapp/MainActivity.kt
@@ -35,7 +35,86 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            //LoginUI(applicationContext)
+            LoginUI(applicationContext)
         }
     }
 }
+
+@Composable
+fun LoginUI(context: Context) {
+    //Variables
+    var email by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    //Diseño
+    Column() {
+
+        //Titulo
+        Text(
+            text = "Iniciar Sesión",
+            fontFamily = FontFamily.SansSerif,
+            textAlign = TextAlign.Center,
+            fontSize = 40.sp,
+            fontWeight = FontWeight.Bold,
+            color = Color.Blue,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 20.dp, top = 20.dp)
+        )
+
+        //TextField EMAIL
+        OutlinedTextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Ingresar Email") },
+            leadingIcon = {
+                Icon(Icons.Default.AccountCircle, contentDescription = "Person")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Email)
+        )
+
+        //TextField PASSWORD
+        OutlinedTextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Ingresar Contraseña") },
+            leadingIcon = {
+                Icon(Icons.Default.Lock, contentDescription = "password")
+            },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp),
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+        )
+
+        //Button Login
+        OutlinedButton(
+            onClick = { logged(email, password, context) },
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(bottom = 10.dp, top = 10.dp)
+        ) {
+            //Text del button
+            Text(
+                text = "login",
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+        }
+    }
+}
+
+//fun Logear
+fun logged(email: String, password: String, context: Context) {
+    if (email == "john@dot.com" && password == "1234") {
+        Toast.makeText(context, "Correcto inicio de sesión", Toast.LENGTH_SHORT).show()
+
+    } else {
+        Toast.makeText(context, "inicio de sesión Fallido", Toast.LENGTH_SHORT).show()
+    }
+}
+
